4378b09a7ca917ae93f784df2929d3c32672c20e
Vikash Mandal <vikashmandal7415@gmail.com>
2024-07-17 23:50:09 +0530

    Updated responsive file

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
 
 const cars = [
     {
-        id:1,
+        id: 1,
         carName: 'Mercedes-Benz S-Class',
-        img:'./Images/mercedes.jpg',
-        carSteeringImg:'./Images/mercedes-steering.jpg',
-        carWheelImg:'./Images/mercedes-wheel.jpg',
-        carLogoImg:'./Images/mercedes-logo.jpg',
+        img: './Images/mercedes.jpg',
+        carSteeringImg: './Images/mercedes-steering.jpg',
+        carWheelImg: './Images/mercedes-wheel.jpg',
+        carLogoImg: './Images/mercedes-logo.jpg',
         carDesc: 'The Mercedes-Benz S-Class combines luxurious elegance with advanced technology, setting the benchmark for premium sedans.',
-        hourPrice:49,
-        dailyPrice:979,
-        monthlyPrice:98989,
+        hourPrice: 49,
+        dailyPrice: 979,
+        monthlyPrice: 98989,
 
-        fuelType:'petrol/diesel',
-        seat:5,
-        average:'10 - 15 km/l',
+        fuelType: 'petrol/diesel',
+        seat: 5,
+        average: '10 - 15 km/l',
 
     },
     {
-        id:2,
+        id: 2,
         carName: 'BMW 7 Series',
-        img:'./Images/bmw.jpg',
-        carSteeringImg:'./Images/bmw-steering.jpg',
-        carWheelImg:'./Images/bmw-wheel.jpg',
-        carLogoImg:'./Images/bmw-back.jpg',
+        img: './Images/bmw.jpg',
+        carSteeringImg: './Images/bmw-steering.jpg',
+        carWheelImg: './Images/bmw-wheel.jpg',
+        carLogoImg: './Images/bmw-back.jpg',
         carDesc: 'The BMW 7 Series epitomizes luxury and performance, offering cutting-edge technology and an exceptional driving experience.',
-        hourPrice:19,
-        dailyPrice:379,
-        monthlyPrice:7599,
-        fuelType:'petrol/diesel',
-        seat:5,
-        average:'12 - 17 km/l',
+        hourPrice: 19,
+        dailyPrice: 379,
+        monthlyPrice: 7599,
+        fuelType: 'petrol/diesel',
+        seat: 5,
+        average: '12 - 17 km/l',
 
 
 
     },
     {
-        id:3,
+        id: 3,
         carName: 'Audi A8',
-        img:'./Images/audi.jpg',
-        carSteeringImg:'./Images/audi-steering.jpg',
-        carWheelImg:'./Images/audi-wheel.jpg',
-        carLogoImg:'./Images/audi-logo.jpg',
+        img: './Images/audi.jpg',
+        carSteeringImg: './Images/audi-steering.jpg',
+        carWheelImg: './Images/audi-wheel.jpg',
+        carLogoImg: './Images/audi-logo.jpg',
         carDesc: 'The Audi A8 blends refined luxury with innovative technology, delivering a sophisticated and dynamic driving experience.',
-        hourPrice:29,
-        dailyPrice:599,
-        monthlyPrice:11599,
-        fuelType:'petrol/diesel',
-        seat:5,
-        average:'12 - 18 km/l',
+        hourPrice: 29,
+        dailyPrice: 599,
+        monthlyPrice: 11599,
+        fuelType: 'petrol/diesel',
+        seat: 5,
+        average: '12 - 18 km/l',
 
 
     },
     {
-        id:4,
+        id: 4,
         carName: 'Tesla Model S',
-        img:'./Images/tesla.jpg',
-        carSteeringImg:'./Images/tesla-back.jpg',
-        carWheelImg:'./Images/tesla-charge.jpg',
-        carLogoImg:'./Images/tesla-logo.jpg',
+        img: './Images/tesla.jpg',
+        carSteeringImg: './Images/tesla-back.jpg',
+        carWheelImg: './Images/tesla-charge.jpg',
+        carLogoImg: './Images/tesla-logo.jpg',
         carDesc: 'The Tesla Model S offers cutting-edge electric performance, futuristic technology, and unmatched efficiency in a sleek, luxurious package.',
-        hourPrice:35,
-        dailyPrice:699,
-        monthlyPrice:13249,
-        fuelType:'Electric',
-        seat:5,
-        average:'2 - 3 km/kWh',
+        hourPrice: 35,
+        dailyPrice: 699,
+        monthlyPrice: 13249,
+        fuelType: 'Electric',
+        seat: 5,
+        average: '2 - 3 km/kWh',
 
 
     },
     {
-        id:5,
+        id: 5,
         carName: 'Porsche Panamera',
-        img:'./Images/porsche.jpg',
-        carSteeringImg:'./Images/porsche-steering.jpg',
-        carWheelImg:'./Images/porsche-back.jpg',
-        carLogoImg:'./Images/porsche-logo.png',
+        img: './Images/porsche.jpg',
+        carSteeringImg: './Images/porsche-steering.jpg',
+        carWheelImg: './Images/porsche-back.jpg',
+        carLogoImg: './Images/porsche-logo.png',
         carDesc: 'The Porsche Panamera combines high-performance driving with luxury and versatility, offering a unique blend of sports car dynamics and executive comfort.',
-        hourPrice:39,
-        dailyPrice:799,
-        monthlyPrice:15599,
-        fuelType:'petrol/diesel',
-        seat:4,
-        average:'11 - 17 km/l',
+        hourPrice: 39,
+        dailyPrice: 799,
+        monthlyPrice: 15599,
+        fuelType: 'petrol/diesel',
+        seat: 4,
+        average: '11 - 17 km/l',
 
 
     },
     {
-        id:6,
+        id: 6,
         carName: 'Rolls Royce Phantom',
-        img:'./Images/rolls-royce.jpg',
-        carSteeringImg:'./Images/Rolls-Royce-steering.jpg',
-        carWheelImg:'./Images/rolls-royce-wheel.jpg',
-        carLogoImg:'./Images/rolls-royce-logo.jpg',
+        img: './Images/rolls-royce.jpg',
+        carSteeringImg: './Images/Rolls-Royce-steering.jpg',
+        carWheelImg: './Images/rolls-royce-wheel.jpg',
+        carLogoImg: './Images/rolls-royce-logo.jpg',
         carDesc: 'The Rolls-Royce Phantom epitomizes ultimate luxury and craftsmanship, providing an unmatched driving experience with its opulent design and advanced features.',
-        hourPrice:49,
-        dailyPrice:979,
-        monthlyPrice:19549,
-        fuelType:'petrol',
-        seat:4,
-        average:'5 - 8 km/l',
+        hourPrice: 49,
+        dailyPrice: 979,
+        monthlyPrice: 19549,
+        fuelType: 'petrol',
+        seat: 4,
+        average: '5 - 8 km/l',
 
 
     },
@@ -120,7 +120,7 @@ const carSteeringImg = document.querySelector('.car-steering_img')
 const carWheelImg = document.querySelector('.car-wheel_img')
 const carLogoImg = document.querySelector('.car-logo_img')
 
-carLinks.forEach((car, index)=>{
+carLinks.forEach((car, index) => {
     car.addEventListener('click', () => {
 
         choosenCar = cars[index];
@@ -140,4 +140,27 @@ carLinks.forEach((car, index)=>{
     })
 
 });
+
+
+const coBtn = document.querySelector('.co-btn')
+const coPage = document.querySelector('.checkout-page')
+const coClose = document.querySelector('.co-close')
+
+coBtn.addEventListener('click', ()=> {
+
+    coPage.style.display = 'flex';
+
+
+})
+
+coClose.addEventListener('click', ()=> {
+
+    coPage.style.display = 'none';
+
+
+})
+
+
+
+
 
